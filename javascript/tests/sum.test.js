83a7b2e713f6c4012d79bdb939cf0454d8407b4a
Andrew Ford <andrew@andrewford.co.nz>
2024-06-17 12:48:11 +1200

    Complete branch

---
+++ javascript/tests/sum.test.js
@@ -0,0 +1,12 @@
+const sum = require("../sum");
+// import sum from "./sum";
+
+describe("sum", () => {
+  it("should return the sum of two positive integers", () => {
+    const input1 = 5;
+    const input2 = 7;
+    const expected = 12;
+    const result = sum(input1, input2);
+    expect(result).toBe(expected);
+  });
+});
