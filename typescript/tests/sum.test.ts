b04f9aeab546f392f9d970145224d0344d63be13
Andrew Ford <andrew@andrewford.co.nz>
2024-06-19 12:08:12 +1200

    Updated more boiler plate

--- typescript/tests/sum.test.ts
+++ typescript/tests/sum.test.ts
@@ -1,5 +1,16 @@
 import { sum } from "../sum";
 
 describe("sum", () => {
-  it("should return the sum of two positive integers", () => {});
+  it("should return the correct sum when input1 and input2 are positive integers", () => {
+      // Arrange
+      const input1 = 1;
+      const input2 = 2;
+      const expected = 3;
+      
+      // Act
+      const actual = sum(input1, input2);
+      
+      // Assert
+      expect(actual).toBe(expected);
+  });
 });
